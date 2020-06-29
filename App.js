6f01dd9a47b7ef7566e752e145b263e9488ceaab
Volter3228 <vova.morozov3228@gmail.com>
2020-06-29 16:54:11 +0300

    Minor updates

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import Navigator from './app/routes/mainStack';
 import {Provider} from 'react-redux';
 import store from './app/redux/store';
 
+/* This is my first React Native App, so
+ * don't judge strictly, I'm trying to do
+ * my best ;^)
+ */
 export default function App() {
   return (
     <Provider store={store}>
